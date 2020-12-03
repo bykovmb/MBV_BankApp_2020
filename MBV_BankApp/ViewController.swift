e99c88ea7bb1afa53d27112eddca53c823b697e8
Mikhail Bykov <bykovmb@gmail.com>
2020-12-04 00:26:39 +0300

    Bank app for iOS

---
+++ MBV_BankApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MBV_BankApp
+//
+//  Created by Mikhail Bykov on 03.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
